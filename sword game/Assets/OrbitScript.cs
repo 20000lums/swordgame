6ba80d6669cbe8f47bc19faa0b5504b12066b619
20000lums <jm686sc@gmail.com>
2023-03-15 10:21:06 -0700

    attempted to add sword swing...

--- sword game/Assets/OrbitScript.cs
+++ sword game/Assets/OrbitScript.cs
@@ -8,17 +8,18 @@ public class OrbitScript : MonoBehaviour
     public GameObject center;
     public float Rotation;
     public float Radious;
+    private float RotationB;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(Mathf.Tan(45));
 
     }
     // tacos are delicious
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.Cos(Rotation)*Radious,Mathf.Sin(Rotation)*Radious,0) + center.transform.position;
-         transform.eulerAngles = new Vector3(0,0,Rotation/Mathf.PI*180 - 90);
+        RotationB = Rotation + 0.5f * Mathf.PI;
+        transform.position = new Vector3(Mathf.Cos(RotationB)*Radious,Mathf.Sin(RotationB)*Radious,0) + center.transform.position;
+         transform.eulerAngles = new Vector3(0,0,RotationB/Mathf.PI*180 - 90);
     }
 }
--- sword game/Assets/playerScript.cs
+++ sword game/Assets/playerScript.cs
@@ -9,10 +9,21 @@ public class playerScript : MonoBehaviour
     public Rigidbody2D myRigidBody;
     public CircleCollider2D myCircleCollider;
     public float jumpHeight;
+    private float direction = 1;
+    private float timer = 0;
+    public float delay = 0;
+    private float swordRotation = 0;
+    public float swordStartingPoint = 0;
+    public float swordEndPoint = 0;
+    public float swingSpeed = 0;
+    private OrbitScript swordOrbit;
+    public GameObject sword;
+    private float attackOn = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = delay;
+        swordOrbit = sword.GetComponent<OrbitScript>();
     }
 
     // Update is called once per frame
@@ -21,15 +32,56 @@ public class playerScript : MonoBehaviour
         if (Input.GetKey(KeyCode.A) == true)
             {
                 myRigidBody.AddForce(Vector2.left*moveSpeed);
+            direction = -1;
             }
         if (Input.GetKey(KeyCode.D) == true)
             {
                 myRigidBody.AddForce(Vector2.right*moveSpeed);
+            direction = 1;
             }
-            if (Input.GetKeyDown(KeyCode.Space) == true && myCircleCollider.IsTouching(flore.GetComponent<BoxCollider2D>()) == true)
+         if (Input.GetKeyDown(KeyCode.Space) == true && myCircleCollider.IsTouching(flore.GetComponent<BoxCollider2D>()) == true)
             {
-                myRigidBody.AddForce(Vector2.up*jumpHeight);
+               myRigidBody.AddForce(Vector2.up*jumpHeight);
             }
-        
+
+        Attack();
+    }
+
+    void Attack()
+    {
+        timer = timer + Time.deltaTime;
+        Debug.Log(timer);
+            if (delay <= timer)
+        {
+         
+            if (Input.GetKeyDown(KeyCode.J) == true)
+            {
+                attackOn = 1;
+                 
+                timer = 0;
+            }
+            else if (Input.GetKeyDown(KeyCode.I) == true)
+            {
+                attackOn = 2;
+                timer = 0;
+                swordRotation = swordStartingPoint;
+            }
+            else if (Input.GetKeyDown(KeyCode.K) == true)
+            {
+                attackOn = 3;
+                timer = 0;
+            }
+
+        }
+        if (attackOn == 2)
+        {
+            swordRotation = swingSpeed * Time.deltaTime;
+            if (swordRotation >= swordEndPoint)
+            {
+                swordRotation = 0;
+                attackOn = 0;
+            }
+        }
+        swordOrbit.Rotation = swordRotation * direction;
     }
 }
