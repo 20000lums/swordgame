b7a167f2f25731c1f0a6d19a513280dda9a52960
20000lums <jm686sc@gmail.com>
2023-03-21 10:19:16 -0700

    enemystarted

--- sword game/Assets/EnemyScript.cs
+++ sword game/Assets/EnemyScript.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class goofyscript : MonoBehaviour
+public class EnemyScript : MonoBehaviour
 {
+    private bool isVulnerable = false;
+    private bool isHit = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isVulnerable = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isVulnerable == true)
+        {
+            
+        }
     }
 }
