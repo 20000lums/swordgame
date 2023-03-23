5b687b4fffd598e1173867df9b748d27cca1ac34
20000lums <jm686sc@gmail.com>
2023-03-23 10:06:16 -0700

    enemy is worked on

--- sword game/Assets/EnemyScript.cs
+++ sword game/Assets/EnemyScript.cs
@@ -6,18 +6,41 @@ public class EnemyScript : MonoBehaviour
 {
     private bool isVulnerable = false;
     private bool isHit = false;
+    public GameObject PlayerObj;
+    public GameObject shieldtop;
+    public GameObject shieldmid;
+    public GameObject shieldbottom;
+    public float ShieldRotation;
+    public float Direction = 1;
+    public float shieldSpread = 0;
+    private float AngToPlayer = 0;
+    private OrbitScript shieldMidOrbit;
+    private OrbitScript shieldTopOrbit;
+    private OrbitScript shieldBottomOrbit;
     // Start is called before the first frame update
     void Start()
     {
         isVulnerable = true;
+        shieldMidOrbit = shieldmid.GetComponent<OrbitScript>();
+        shieldTopOrbit = shieldtop.GetComponent<OrbitScript>();
+        shieldBottomOrbit = shieldbottom.GetComponent<OrbitScript>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        if(isVulnerable == true)
+        AngToPlayer = Mathf.Atan2(transform.position.y - PlayerObj.transform.position.y, transform.position.x - PlayerObj.transform.position.x);
+        calcVulnerable();
+    }
+    void calcVulnerable()
+    {
+        if (isVulnerable == true)
         {
-            
+            ShieldRotation = AngToPlayer;
+            Direction = Mathf.Abs(AngToPlayer) / AngToPlayer;
+            shieldMidOrbit.Rotation = ShieldRotation;
+            shieldTopOrbit.Rotation = ShieldRotation + shieldSpread * Direction;
+            shieldBottomOrbit.Rotation = ShieldRotation - shieldSpread * Direction;
         }
     }
 }
